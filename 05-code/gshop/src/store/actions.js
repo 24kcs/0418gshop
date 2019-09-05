9b17756f83b7f8fb59fa18c9c5de785be51b6375
24kcs <younghongbo@126.com>
2019-09-05 11:58:51 +0800

    update-code

---
+++ 05-code/gshop/src/store/actions.js
@@ -0,0 +1,11 @@
+// 包含了多个间接修改状态数据的方法的对象
+// 引入常量名-----mutation的types
+// import { RECEIVE_ADDRESS, RECEIVE_CATEGORYS, RECEIVE_SHOPS, RESET_USER, RECEIVE_GOODS, RECEIVE_RATINGS, RECEIVE_INFO } from './mutation-types.js'
+// 引入api的相关接口
+// import { reqAddress, reqCategorys, reqShops, reqLoginOut, reqGoods, reqRatings, reqInfo } from '../api'
+export default {
+  
+  // xxx() {
+  //   console.log('我是总的actions中的xxx方法')
+  // }
+}
